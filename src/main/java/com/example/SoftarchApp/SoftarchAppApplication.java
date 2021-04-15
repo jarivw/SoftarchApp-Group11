52c324573a8777669e1d3ff9fef6665139ad6540
jarivw <jarivwijk@gmail.com>
2021-04-15 11:39:53 +0200

    first

---
+++ src/main/java/com/example/SoftarchApp/SoftarchAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.SoftarchApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftarchAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftarchAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SoftarchApp/SoftarchAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SoftarchApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftarchAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
