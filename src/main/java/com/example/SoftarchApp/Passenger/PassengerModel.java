19d2836e839b07d39838dfc2624fd0651ff4f0d0
jarivw <jarivwijk@gmail.com>
2021-04-15 12:54:01 +0200

    Passenger renamed

--- src/main/java/com/example/SoftarchApp/Passenger/PassengerModel.java
+++ src/main/java/com/example/SoftarchApp/Passenger/PassengerModel.java
@@ -1,4 +1,14 @@
 package com.example.SoftarchApp.Passenger;
 
+import java.util.UUID;
+
 public class PassengerModel {
+
+    private final UUID passengerid;
+    private final String name;
+
+    public PassengerModel(UUID passengerid, String name){
+        this.passengerid = passengerid;
+        this.name = name;
+    }
 }
